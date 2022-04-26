06bcd4054bd989852d066230ee3e72cd1cbc492e
Hugo Calmels <calmels.hugo@yahoo.fr>
2022-04-26 06:47:47 +0200

    first commit

---
+++ src/redux/store.js
@@ -0,0 +1,8 @@
+import { configureStore } from '@reduxjs/toolkit';
+import usersReducer from '../redux/features/users/usersSlice'
+
+export const store = configureStore({
+  reducer: {
+    users: usersReducer,
+  }
+})
